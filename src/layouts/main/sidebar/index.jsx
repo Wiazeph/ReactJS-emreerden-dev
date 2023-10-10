7dd6b4f3426ee2fde8ef0b302c1e7b6fd7b1c48e
Emre Erden <emreerden@pm.me>
2023-10-10 20:35:50 +0300

    style: add color conditions

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 import navLinks from "~/utils/consts";
 
@@ -5,14 +6,22 @@ const SideBar = () => {
   return (
     <nav className="flex flex-col gap-4">
       {navLinks.map((nlink, index) => (
-        <NavLink
-          key={index}
-          to={nlink.path}
-          className={`flex gap-4 items-center hover:text-[${nlink.color}] transition-colors w-fit`}
-        >
-          <div>{index + 1}.</div>
-          {/* <div className="h-3 w-px bg-current"></div> */}
-          <div># {nlink.title}</div>
+        <NavLink key={index} to={nlink.path} className="w-fit">
+          {({ isActive }) => (
+            <div
+              className={classNames(
+                "flex gap-4 items-center transition-colors",
+                {
+                  [nlink.color]: isActive,
+                  [`${nlink.hoverColor}`]: !isActive,
+                }
+              )}
+            >
+              <div>{index + 1}.</div>
+              {/* <div className="h-3 w-px bg-current"></div> */}
+              <div># {nlink.title}</div>
+            </div>
+          )}
         </NavLink>
       ))}
     </nav>
--- src/utils/consts.jsx
+++ src/utils/consts.jsx
@@ -2,23 +2,32 @@ const navLinks = [
   {
     path: "/",
     title: "hello",
-    color: "rgb(249,168,212)",
+    color: "text-pink-500",
+    hoverColor: "hover:text-pink-300",
   },
   {
     path: "/whoami",
     title: "whoami",
+    color: "text-yellow-500",
+    hoverColor: "hover:text-yellow-300",
   },
   {
     path: "/skills",
     title: "skills",
+    color: "text-green-500",
+    hoverColor: "hover:text-green-300",
   },
   {
     path: "/portfolio",
     title: "portfolio",
+    color: "text-blue-500",
+    hoverColor: "hover:text-blue-300",
   },
   {
     path: "/tools",
     title: "tools",
+    color: "text-purple-500",
+    hoverColor: "hover:text-purple-300",
   },
 ];
 
