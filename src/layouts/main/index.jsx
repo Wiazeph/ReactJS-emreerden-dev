ad8ce0b043c5a67e3f09ea3b1932293c28c926f8
Emre Erden <emreerden@pm.me>
2023-10-10 03:29:29 +0300

    style: create layouts structure

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -3,9 +3,11 @@ import SideBar from "./sidebar";
 
 const MainLayout = () => {
   return (
-    <div>
-      <SideBar />
-      <main>
+    <div className="container mx-auto flex flex-col sm:flex-row gap-10 sm:gap-0 items-center bg-red-900 h-screen px-6 pt-10 pb-14">
+      <aside className="bg-blue-900 sm:max-w-[300px] w-full">
+        <SideBar />
+      </aside>
+      <main className="bg-green-900 w-full">
         <Outlet />
       </main>
     </div>
