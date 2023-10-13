bc93028deced3b07fd93c9fe91cd532dbc71856e
Emre Erden <emreerden@pm.me>
2023-10-13 04:51:30 +0300

    feat: add Google Caveat Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ export default {
     extend: {
       container: {
         screens: {
-          md: "768px",
           lg: "1024px",
           xl: "1280px",
         },
+      },
+      fontFamily: {
+        Caveat: ["Caveat", "cursive"],
       },
     },
   },
